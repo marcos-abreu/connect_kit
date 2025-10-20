990404120c81b499b1262ef4d2485112e61a936b
marcos-abreu <abreu.marcos@gmail.com>
2025-10-20 10:06:41 -0400

    fix(example): use the new instance singleton to access the plugin in the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,7 +30,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion = await ConnectKit.getPlatformVersion() ?? 'Unknown';
+      platformVersion =
+          await ConnectKit.instance.getPlatformVersion() ?? 'Unknown';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
