be3b15e0fca827983a8f648831dca19c817ed25e
marcos-abreu <abreu.marcos@gmail.com>
2025-11-11 12:33:32 -0500

    chore(exports): sync model exports

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,8 +1,26 @@
-// Record types
+// Health Record Schema (useful reading writeing health records)
 export 'package:connect_kit/src/models/schema/ck_type.dart';
+export 'package:connect_kit/src/models/schema/ck_source.dart';
+export 'package:connect_kit/src/models/schema/ck_device.dart';
 
-// Response types
+// Health Records (Quantity and Category) types
+export 'package:connect_kit/src/models/records/ck_data_record.dart';
+export 'package:connect_kit/src/models/records/ck_record_builder.g.dart';
+
+// Health Records Specialized types
+export 'package:connect_kit/src/models/records/ck_audiogram.dart';
+export 'package:connect_kit/src/models/records/ck_blood_pressure.dart';
+export 'package:connect_kit/src/models/records/ck_ecg.dart';
+export 'package:connect_kit/src/models/records/ck_nutrition.dart';
+export 'package:connect_kit/src/models/records/ck_sleep_session.dart';
+export 'package:connect_kit/src/models/records/ck_workout.dart';
+
+// Special Models to represent data state and type
 export 'package:connect_kit/src/models/ck_sdk_status.dart';
 export 'package:connect_kit/src/models/ck_access_status.dart';
 export 'package:connect_kit/src/models/ck_permission_status.dart';
 export 'package:connect_kit/src/models/ck_access_type.dart';
+export 'package:connect_kit/src/models/ck_write_result.dart';
+
+// categories enums
+export 'package:connect_kit/src/models/ck_categories.dart';
